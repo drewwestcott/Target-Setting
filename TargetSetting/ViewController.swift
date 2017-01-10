0b050351c0a8baf7234ec8dd4cfd8f4eefb5c9ee
Drew Westcott <code@drewwestcott.co.uk>
2017-01-10 23:36:49 +0000

    Code Kata: dismiss the keyboard when editing finished

--- TargetSetting/ViewController.swift
+++ TargetSetting/ViewController.swift
@@ -8,11 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, UITextFieldDelegate {
+	@IBOutlet weak var targetField: UITextField!
+
+	let localStorage = UserDefaults.standard
+	var target = 10000
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		targetField.delegate = self
+		
+		if let storedTarget = localStorage.object(forKey: "target") as? Int {
+			targetField.placeholder = "\(storedTarget)"
+			target = storedTarget
+		}
 	}
 
 	override func didReceiveMemoryWarning() {
@@ -20,6 +31,29 @@ class ViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
+	func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+		textField.resignFirstResponder()
+		return true
+	}
 
+	func textFieldDidEndEditing(_ textField: UITextField) {
+		textField.resignFirstResponder()
+	}
+	
+	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+		view.endEditing(true)
+		super.touchesBegan(touches, with: event)
+	}
+	
+	@IBAction func setTargetTapped(_ sender: Any) {
+		if targetField.text != "" && targetField.text != nil {
+			targetField.resignFirstResponder()
+			target = Int(targetField.text!)!
+			localStorage.set(target, forKey: "target")
+		} else {
+			// display a warning?
+		}
+	}
+	
 }
 
